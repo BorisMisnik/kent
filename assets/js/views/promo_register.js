c71a3514b55aaeffcd53560e3111b55678075497
Boris <bmisnik@ua-kv-w-26.cee.grey.global>
2013-10-01 12:04:38 +0300

    hdi mobile

--- assets/js/views/promo_register.js
+++ assets/js/views/promo_register.js
@@ -119,10 +119,10 @@ define(
                                         }
                                         else {
                                             if ( navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile/) ){
-                                                window.location.href = '/main-mobile.html#gallery';
+                                                window.location.href = '/main-mobile.html';
                                             }
                                             else {
-                                                window.location.href = '/main.html#gallery';
+                                                window.location.href = '/main.html';
                                             }
                                         }
                                     });
